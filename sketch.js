22de76db2680b98f8cfba8e5dc24ee6c0ba2f72b
ttmmd55 <chenshuting47@gmail.com>
2023-02-12 23:53:19 +0000

    change

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 let blueFact;
 let loading = true;
-// let img;
 let data = {};
 let words =[]
 let wordData;
@@ -10,21 +9,16 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 600);
   background(0);
   let wordData = data.content.text[floor(random(0, 10))];
   console.log(wordData);
   blueFact = wordData;
-
-  fill(10,255,255);
-  textAlign(CENTER);
-  textSize(30);
-  text(blueFact,0,0, width);
-  
 }
 
 function draw() {
-  
-
-
+  fill(10,255,255);
+  textAlign(CENTER);
+  textSize(25);
+  text(blueFact,0,0, width);
 }
