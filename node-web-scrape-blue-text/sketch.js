71b98295e586f99a4738243356c8ba234d24c713
ttmmd55 <chenshuting47@gmail.com>
2023-02-12 22:12:06 +0000

    change

--- node-web-scrape-blue-text/sketch.js
+++ node-web-scrape-blue-text/sketch.js
@@ -10,18 +10,18 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
-  canvas.parent("sketch-container"); //move our canvas inside this HTML element
   background(0);
   let wordData = data.content.text[floor(random(0, 200))];
   console.log(wordData);
   blueFact = wordData;
 
-  fill(10,10,250);
+  fill(10,255,250);
   textAlign(CENTER);
   textSize(20);
   text(blueFact, 50, 150, 400);
+
 }
 
 function draw() {
-
+  
 }
