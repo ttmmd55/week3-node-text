badfbdf6ca38de6e2df1ae3b9f1cbaddd5b67b18
ttmmd55 <chenshuting47@gmail.com>
2023-02-12 13:06:46 +0000

    change

--- node-web-scrape-blue-text/sketch.js
+++ node-web-scrape-blue-text/sketch.js
@@ -1,32 +1,27 @@
-let plantFact;
+let blueFact;
 let loading = true;
-// let img;
 let data = {};
 let words =[]
 let wordData;
+
 function preload() {
   data = loadJSON('./json/WebScraping.json')
 }
 
-
-
 function setup() {
   createCanvas(500, 500);
   background(250);
-  let wordData = data.content.text[floor(random(0, 310))];
+  let wordData = data.content.text[floor(random(0, 200))];
 
-  
+
   console.log(wordData);
-  plantFact = wordData;
-
-  textAlign(CENTER);
-  textSize(30);
-  text(plantFact, 0, 0, width);
-  
+  blueFact = wordData;
 }
 
 function draw() {
   
-
+  textAlign(CENTER);
+  textSize(30);
+  text(blueFact, 0, 0, width);
 
 }
